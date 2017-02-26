3485f440d8f1c53b716138afbe5f2eb707dcd2df
Julian Ladjani <julian.ladjani@epitech.eu>
2017-02-26 06:24:44 +0100

    unlimitedpower

--- src/ia_foncs2.c
+++ src/ia_foncs2.c
@@ -5,7 +5,7 @@
 ** Login   <julian.ladjani@epitech.net>
 ** 
 ** Started on  Sun Feb 26 04:28:42 2017 julian ladjani
-** Last update Sun Feb 26 06:11:45 2017 julian ladjani
+** Last update Sun Feb 26 06:24:25 2017 julian ladjani
 */
 
 #include "matchstick.h"
@@ -69,13 +69,13 @@ void    ia_case3(t_game *game, int *tline, int *tstick)
 
   line = *tline;
   stick = *tstick;
-  if (game->nbline % 2 == 0)
+  if (game->nbline % 2 == 0 && game->line[line] < game->maxtake)
     stick = game->line[line];
   while (check_line_withedit(game, line, stick) == 0
 	 && stick <= game->line[line] && stick < game->maxtake
 	 && stick > 0)
     {
-      if (game->nbline % 2 == 0)
+      if (game->nbline % 2 == 0 && game->line[line] < game->maxtake)
 	stick--;
       else
 	stick++;
